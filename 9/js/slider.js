f8e312cbca31a8f2eeeeb6b8d7fd7519a690e045
Keks <keks@htmlacademy.ru>
2022-08-25 22:09:06 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/slider.js
@@ -0,0 +1,1 @@
+const container=document.querySelector(".example__slider-wrapper");document.querySelector(".example__range").addEventListener("input",(e=>{container.style.setProperty("--position",`${e.target.value}%`)}));
